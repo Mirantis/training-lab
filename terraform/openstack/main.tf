8697f49b73d48dbb1f6700af155182fb22fcaf6f
Petr Ruzicka <petr.ruzicka@gmail.com>
2018-06-13 09:46:43 +0200

    Provisioning of VMs for OpenStack added

--- terraform/openstack/main.tf
+++ terraform/openstack/main.tf
@@ -6,7 +6,73 @@ provider "openstack" {
   user_name   = "${var.openstack_user_name}"
 }
 
+data "openstack_networking_network_v2" "external_network" {
+  name = "${var.openstack_networking_network_external_network_name}"
+}
+
+# Create Keypair
 resource "openstack_compute_keypair_v2" "keypair" {
-  name       = "${var.openstack_compute_keypair_name}"
+  name       = "${var.prefix}-keypair"
   public_key = "${file(var.openstack_compute_keypair_public_key)}"
+}
+
+# Create private network
+resource "openstack_networking_network_v2" "private-network" {
+  name = "${var.prefix}-network"
+  admin_state_up = "true"
+}
+
+# Create private subnet
+resource "openstack_networking_subnet_v2" "private-subnet" {
+  name = "${var.prefix}-subnet"
+  network_id = "${openstack_networking_network_v2.private-network.id}"
+  cidr       = "${var.openstack_networking_subnet_cidr}"
+  dns_nameservers = "${var.openstack_networking_subnet_dns_nameservers}"
+  enable_dhcp = true
+}
+
+# Create router for private subnet
+resource "openstack_networking_router_v2" "private-router" {
+  name = "${var.prefix}-router"
+  external_network_id = "${data.openstack_networking_network_v2.external_network.id}"
+  admin_state_up = "true"
+}
+
+# Create router interface for private subnet
+resource "openstack_networking_router_interface_v2" "router-interface" {
+  router_id = "${openstack_networking_router_v2.private-router.id}"
+  subnet_id = "${openstack_networking_subnet_v2.private-subnet.id}"
+}
+
+# Create floating IP for nodes
+resource "openstack_networking_floatingip_v2" "floatingips" {
+    count = "${var.vm_count * var.environment_count}"
+    pool = "${var.openstack_networking_floatingip}"
+}
+
+# Associate floating IP with nodes
+resource "openstack_compute_floatingip_associate_v2" "floatingips-associate" {
+    count = "${var.vm_count * var.environment_count}"
+    floating_ip = "${element(openstack_networking_floatingip_v2.floatingips.*.address, count.index)}"
+    instance_id = "${element(openstack_compute_instance_v2.vms.*.id, count.index)}"
+    fixed_ip = "${element(openstack_compute_instance_v2.vms.*.network.0.fixed_ip_v4, count.index)}"
+}
+
+# Create nodes
+resource "openstack_compute_instance_v2" "vms" {
+    count = "${var.vm_count * var.environment_count}"
+    name = "${format("%s-%02d-kvm%02d", var.prefix, count.index % var.environment_count + 1, count.index / var.environment_count + 1)}"
+    image_name = "${var.openstack_compute_instance_image_name}"
+    flavor_name = "${var.openstack_compute_instance_flavor_name}"
+    key_pair = "${openstack_compute_keypair_v2.keypair.name}"
+
+    network {
+        uuid = "${openstack_networking_network_v2.private-network.id}"
+        access_network = true
+    }
+}
+
+output "VMs" {
+    description = "VMs with IPs"
+    value = "${zipmap(openstack_compute_instance_v2.vms.*.name, openstack_networking_floatingip_v2.floatingips.*.address)}"
 }
--- terraform/openstack/variables.tf
+++ terraform/openstack/variables.tf
@@ -1,18 +1,58 @@
-variable "openstack_auth_url" {
+variable "environment_count" {
+  default = 1
 }
 
-variable "openstack_compute_keypair_name" {
+variable "openstack_auth_url" {
+  description = "The endpoint url to connect to OpenStack"
+}
+
+variable "openstack_compute_instance_flavor_name" {
+}
+
+variable "openstack_compute_instance_image_name" {
 }
 
 variable "openstack_compute_keypair_public_key" {
   default = "~/.ssh/id_rsa.pub"
 }
 
+variable "openstack_networking_network_external_network_name" {
+  default = "public"
+}
+
+variable "openstack_networking_subnet" {
+  default = "192.168.0.0/24"
+}
+
+variable "openstack_networking_subnet_cidr" {
+  default = "192.168.1.1/24"
+}
+
+variable "openstack_networking_subnet_dns_nameservers" {
+  default = ["1.1.1.1", "8.8.8.8"]
+}
+
+variable "openstack_networking_floatingip" {
+  default = "public"
+}
+
 variable "openstack_password" {
+  description = "The password for the Tenant"
 }
 
 variable "openstack_tenant_name" {
+  description = "The name of the Tenant"
 }
 
 variable "openstack_user_name" {
+  description = "The username for the Tenant"
+}
+
+variable "prefix" {
+  default = "terraform"
+}
+
+variable "vm_count" {
+  description = "Number of VMs which should be created"
+  default = 3
 }
